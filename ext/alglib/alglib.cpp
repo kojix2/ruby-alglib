83f5655b131c002ab19db132e87648ced43610a6
kojix2 <2xijok@gmail.com>
2023-10-18 12:53:44 +0900

    Improve extconf.rb [wip]

--- ext/alglib/alglib.cpp
+++ ext/alglib/alglib.cpp
@@ -2,14 +2,50 @@
 
 #include <rice/rice.hpp>
 #include <rice/stl.hpp>
-#include "numo.hpp"
+// #include "numo.hpp"
 #include "statistics.h"
 
 using namespace Rice;
 
+// convert Ruby Array to real_1d_array
+alglib::real_1d_array to_real_1d_array(Array ary)
+{
+    alglib::real_1d_array a;
+    a.setlength(ary.size());
+    int count = 0;
+    Array::iterator it = ary.begin();
+    Array::iterator end = ary.end();
+    for (; it != end; ++it)
+    {
+        a[count] = detail::From_Ruby<double>().convert(it->value());
+        count++;
+    }
+    return a;
+}
+
+Hash rb_studentttest1(
+    Object self,
+    Array x,
+    int n,
+    double mean)
+{
+    Hash result;
+    alglib::real_1d_array a = to_real_1d_array(x);
+    // output parameters
+    double bothtails;
+    double lefttail;
+    double righttail;
+
+    alglib::studentttest1(a, n, mean, bothtails, lefttail, righttail);
+    result["bothtails"] = bothtails;
+    result["lefttail"] = lefttail;
+    result["righttail"] = righttail;
+    return result;
+}
 
 extern "C" void Init_alglib()
 {
-  Module rb_mAlglib =
-      define_module("Alglib");
+    Module rb_mAlglib =
+        define_module("Alglib")
+            .define_module_function("statistics_covariance", &rb_studentttest1);
 }
--- ext/alglib/extconf.rb
+++ ext/alglib/extconf.rb
@@ -1,18 +1,24 @@
 # frozen_string_literal: true
 
 require 'mkmf-rice'
-require 'numo/narray'
+# require 'numo/narray'
 
 vendor = File.expand_path('../../vendor', __dir__)
+vendor_src = File.join(vendor, 'alglib-cpp/src')
 
-apple_clang = RbConfig::CONFIG['CC_VERSION_MESSAGE'] =~ /apple clang/i
+# apple_clang = RbConfig::CONFIG['CC_VERSION_MESSAGE'] =~ /apple clang/i
 
 # numo-narray
-numo = File.join(Gem.loaded_specs['numo-narray'].require_path, 'numo')
-abort 'Numo header not found' unless find_header('numo/narray.h', numo)
-abort 'Numo library not found' if Gem.win_platform? && !find_library('narray', nil, numo)
-find_header('numo.hpp', File.expand_path('../../include', __dir__))
+# numo = File.join(Gem.loaded_specs['numo-narray'].require_path, 'numo')
+# abort 'Numo header not found' unless find_header('numo/narray.h', numo)
+# abort 'Numo library not found' if Gem.win_platform? && !find_library('narray', nil, numo)
+# find_header('numo.hpp', File.expand_path('../../include', __dir__))
 
-find_header('statistics.h', File.join(vendor, 'alglib-cpp/src'))
+dir_config 'alglib', vendor_src, vendor_src
+
+$INCFLAGS << " -I#{vendor_src}"
+$VPATH << vendor_src
+
+$objs = Dir.glob(vendor_src + '/*.cpp').map { |f| f.sub(/\.cpp$/, ".o") }
 
 create_makefile('alglib/alglib')
