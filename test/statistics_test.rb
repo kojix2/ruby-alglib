3bedf7ce85c462968651b458cc87f13edc0cac44
kojix2 <2xijok@gmail.com>
2023-10-20 15:31:27 +0900

    Add tests

--- test/statistics_test.rb
+++ test/statistics_test.rb
@@ -11,19 +11,47 @@ class AlglibTest < Test::Unit::TestCase
     assert_equal r('mean(c(1.1, 2.2, 3.3, 4.4))').to_f, Alglib.sample_mean([1.1, 2.2, 3.3, 4.4])
   end
 
-  def test_sample_mean_vs_rlang
-    arr = Array.new(10) { rand(100) }
-    assert_equal r("mean(c(#{arr.join(',')}))").to_f, Alglib.sample_mean(arr)
+  def test_sample_mean_random
+    arr = Array.new(10) { rand(100) / 10.0 }
+    assert_in_delta r("mean(c(#{arr.join(',')}))").to_f, Alglib.sample_mean(arr), 0.001
   end
 
   def test_sample_variance
     assert_equal r('var(c(1, 2))').to_f, Alglib.sample_variance([1, 2])
     assert_equal r('var(c(1, 2, 3))').to_f, Alglib.sample_variance([1, 2, 3])
-    assert_in_delta r('var(c(1.1, 2.2, 3.3, 4.4))').to_f, Alglib.sample_variance([1.1, 2.2, 3.3, 4.4]), 0.0001
+    assert_in_delta r('var(c(1.1, 2.2, 3.3, 4.4))').to_f, Alglib.sample_variance([1.1, 2.2, 3.3, 4.4]), 0.001
   end
 
-  def test_sample_variance_vs_rlang
-    arr = Array.new(10) { rand(100) }
-    assert_in_delta r("var(c(#{arr.join(',')}))").to_f, Alglib.sample_variance(arr), 0.001
+  def test_sample_variance_random
+    5.times do
+      arr = Array.new(10) { rand(100) / 10.0 }
+      assert_in_delta r("var(c(#{arr.join(',')}))").to_f, Alglib.sample_variance(arr), 0.001
+    end
+  end
+
+  def test_sample_skewness
+    assert_in_delta r('library(e1071)', 'skewness(c(1, 2))').to_f, Alglib.sample_skewness([1, 2]), 0.001
+    assert_in_delta r('library(e1071)', 'skewness(c(1, 2, 3))').to_f, Alglib.sample_skewness([1, 2, 3]), 0.001
+    assert_in_delta r('library(e1071)', 'skewness(c(1.1, 2.2, 3.3, 4.4))').to_f, Alglib.sample_skewness([1.1, 2.2, 3.3, 4.4]), 0.001
+  end
+
+  def test_sample_skewness_random
+    5.times do
+      arr = Array.new(10) { rand(100) / 10.0 }
+      assert_in_delta r('library(e1071)', "skewness(c(#{arr.join(',')}))").to_f, Alglib.sample_skewness(arr), 0.001
+    end
+  end
+
+  def test_sample_kurtosis
+    assert_in_delta r('library(e1071)', 'kurtosis(c(1, 2))').to_f, Alglib.sample_kurtosis([1, 2]), 0.001
+    assert_in_delta r('library(e1071)', 'kurtosis(c(1, 2, 3))').to_f, Alglib.sample_kurtosis([1, 2, 3]), 0.001
+    assert_in_delta r('library(e1071)', 'kurtosis(c(1.1, 2.2, 3.3, 4.4))').to_f, Alglib.sample_kurtosis([1.1, 2.2, 3.3, 4.4]), 0.001
+  end
+
+  def test_sample_kurtosis_random
+    5.times do
+      arr = Array.new(10) { rand(100) / 10.0 }
+      assert_in_delta r('library(e1071)', "kurtosis(c(#{arr.join(',')}))").to_f, Alglib.sample_kurtosis(arr), 0.001
+    end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,12 +2,20 @@
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'alglib'
-
+require 'open3'
 require 'test-unit'
 
 # Run code in Rscript
-def r(code)
-  result = `Rscript -e '#{code}'`.strip
-  # Remove line number from the begining of line (e.g. "[1] 1.5")
-  result.sub(/^\[\d+\]\s+/, '')
+def r(*code)
+  cmd = "Rscript #{code.map { |c| "-e '#{c}'" }.join(' ')}"
+
+  stdout, stderr, status = Open3.capture3(cmd)
+  unless status.success?
+    raise "Rscript failed: #{stderr}"
+  end
+
+  stdout.sub(/^\[\d+\]\s+/, '')
 end
+
+# install e1071 package if it's not installed
+r('if (!requireNamespace("e1071")) install.packages("e1071")')
